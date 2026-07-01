1c3af93a41e436f5cb5e4c2942e1fa18cecbd2aa
miguelgomes-lab <miguel.gomes@tecnimoove.pt>
2026-07-01 10:48:55 +0100

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: false,
+  },
+}
 module.exports = nextConfig
